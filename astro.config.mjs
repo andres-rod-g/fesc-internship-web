1cc86c0f4af2585d20e5b18262922c5bffd08c1a
Andrés Felipe Rodríguez Gallego <79536152+andres-rod-g@users.noreply.github.com>
2025-07-30 17:50:03 -0500

    Add Astro Node adapter and update dependencies
    Configured Astro to use the Node adapter in standalone mode for server output. Updated @astrojs/react, @astrojs/tailwind, and astro dependencies to latest versions and added @astrojs/node to package.json.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 export default defineConfig({
   output: "server",
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
