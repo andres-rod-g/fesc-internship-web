146effd0847249dfe0b8991d6937eb94d84f5a5b
juansegaliz <juansegaliz@gmail.com>
2025-07-31 15:04:08 -0500

    se aplica que db.js tome la variable de ambiente process.env.DB_NAME

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const MONGO_URL = process.env.MONGODB_URI || "mongodb://localhost:27017";
-const DB_NAME = "fesc_platform";
+const DB_NAME = process.env.DB_NAME || "fesc_platform";
 
 let client;
 let db;
